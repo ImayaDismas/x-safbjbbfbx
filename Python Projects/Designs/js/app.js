6e7accebca9d7c0b1f5d178cabfe53153a7570e1
ImayaDismas <imayadismas@gmail.com>
2016-09-11 07:47:59 +0300

    Edited the pages and added new features

--- Python Projects/Designs/js/app.js
+++ Python Projects/Designs/js/app.js
@@ -101,3 +101,48 @@ $(document).ready(function () {
     resizeElements();
   });
 //End of the search bar
+
+
+
+//dropdown js
+
+var $container = $('.dropdown-menu'),
+    $list = $('.dropdown-menu ul'),
+    listItem = $list.find('li');
+
+$(".dropdown1 .title").click(function () {
+  if( $container.height() > 0) {
+    closeMenu(this);
+  } else {
+    openMenu(this);
+  }
+});
+
+$(".dropdown-menu li").click(function () {
+  closeMenu(this);
+});
+
+function closeMenu(el) {
+  $(el).closest('.dropdown1').toggleClass("closed").find(".title").text($(el).text());
+  $container.css("height", 0);
+  $list.css( "top", 0 );
+}
+
+function openMenu(el) {
+  $(el).parent().toggleClass("closed");
+  
+  $container.css({
+    height: 200
+  })
+  .mousemove(function(e) {
+    var heightDiff = $list.height() / $container.height(),
+        offset = $container.offset(),
+        relativeY = (e.pageY - offset.top),
+        top = relativeY*heightDiff > $list.height()-$container.height() ?
+              $list.height()-$container.height() : relativeY*heightDiff;
+
+    $list.css("top", -top);
+  });
+}
+
+//end of dropdown js
