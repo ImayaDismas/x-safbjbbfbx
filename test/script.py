6ba0fff09c5509a8c9e45635e3bd939e5c35f5f4
ImayaDismas <imayadismas@gmail.com>
2016-11-11 18:56:15 +0300

    Able to pass variables between scripts in test folder

---
+++ test/script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from script2 import Testg
+
+class Testgui:
+    def passVar():
+        # from script2 import x
+        print("New output: " + Testg.y)
+
+        
+
+    print(Testg.y)
+    passVar()
---
+++ test/script2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+x = ""
+class Testg:
+
+    def passVars():
+        j = x + "Hello world"
+        # from script2 import x
+        # print(x)
+        return j
+
+
+    y = passVars()
