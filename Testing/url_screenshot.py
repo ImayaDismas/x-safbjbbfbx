34a1acc75881001be578f2b3ec4d3924167c6630
ImayaDismas <imayadismas@gmail.com>
2017-02-02 18:57:27 +0300

    Created a project for testing modules

---
+++ Testing/url_screenshot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+from PyQt5.QtCore import *
+from PyQt5.QtWebKit import *
+from PyQt5.QtWebKitWidgets import *
+from PyQt5.QtWidgets import QApplication
+
+app = QApplication(sys.argv)
+
+web_url = "https://pythonspot.com"
+web = QWebView()
+web.load(QUrl(web_url))
+web.show()
+
+sys.exit(app.exec_())
